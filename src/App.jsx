ee28ddb6d71a75dc72720c4d3694a6e75e4774dd
guido2288 <guido2288@gmail.com>
2023-11-11 13:50:44 -0300

    Detectar al ganador

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,56 @@ const Square = ({children , isSelected , updateBoard , index}) => {
   )
 };
 
+const WINNER_COMBOS = [
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+
+]
+
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  const [winner, setWinner] = useState(null); //null es que no hay ganador, false es que hay un empate
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a , b  , c] = combo;
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] 
+      ) {
+        return boardToCheck[a]
+      }
+    }
+
+    return null
+  }
+
+
+
   const updateBoard = (index) => {
+
+    if(board[index] || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard);
+    if(newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
@@ -55,6 +93,33 @@ function App() {
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <section>
+        {
+          winner !== null && (
+            <section className="winner">
+              <div className="text">
+                <h2>
+                  {
+                    winner === false
+                      ? 'Empate'
+                      : 'Ganó:' 
+                  }
+                </h2>
+
+                  <header className="win">
+                    {winner && <Square>{winner}</Square>}
+                  </header>
+
+                  <footer>
+                    <button>Empezar de nuevo</button>
+                  </footer>
+
+              </div>
+            </section>
+          )
+        }
       </section>
     </main>
   )
